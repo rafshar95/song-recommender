3e2668c0d4ac68f6d45832950fee70af54877c6b
Evrim Ozel <eozel@uci.edu>
2021-06-09 09:17:22 -0700

    eval

--- eval.py
+++ eval.py
@@ -7,23 +7,26 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics.pairwise import cosine_similarity
 import heapq
 import csv
+import sys
 
 
-def rank_songs(playlists_with_embeddings, dist, p_seed, index_id, num_recs, measure='max'):
+def rank_songs(playlists_with_embeddings, dist, seed, index_id, num_recs, measure='max'):
     # measure can be {'avg', 'max'}
-    songs = playlists_with_embeddings.drop_duplicates(['artist', 'track', 'embedding'], keep='last')
-    for p in p_seed:
-        songs = songs[songs['sid'] != p[index_id]]
+    songs = playlists_with_embeddings.drop_duplicates(['artist', 'track'], keep='last')
+
+    for song in seed:
+        songs = songs[songs['sid'] != song[index_id]]
     scores = []
 
     def compute_score(row):
         if measure == 'avg':
-            a = statistics.mean([dist[row['sid']][seed[index_id]] for seed in p_seed])
+            a = statistics.mean([dist[row['sid']][song[index_id]] for song in seed])
             scores.append((a, row['sid']))
 
         elif measure == 'max':
-            a = max([dist[row['sid']][seed[index_id]] for seed in p_seed])
+            a = max([dist[row['sid']][song[index_id]] for song in seed])
             scores.append((a, row['sid']))
+
     songs.apply(compute_score, axis=1)
     sorted_songs = heapq.nlargest(num_recs, scores)
     filtered_songs = pd.DataFrame(columns=songs.columns)
@@ -33,19 +36,18 @@ def rank_songs(playlists_with_embeddings, dist, p_seed, index_id, num_recs, meas
     return filtered_songs
 
 
-def eval_playlist(ranked_songs, p_test, index_artist, index_track):
-    # returns [precision@10, precision@50, NDCG@100]
+def eval_playlist(ranked_songs, test, index_sid):
+    # returns [recall@10, recall@50, NDCG@100]
     result = [0, 0, 0]
-    ranked_songs.set_index(['artist', 'track'])
 
     rel = [False] * 100
-    for i in p_test:
-        found = ((ranked_songs[:100]['artist'] == i[index_artist]) & (ranked_songs[:100]['track'] == i[index_track])).values.tolist()
+    for i in test:
+        found = (ranked_songs[:100]['sid'] == i[index_sid]).values.tolist()
         rel = [i or j for i, j in zip(found, rel)]
 
-    # precision@10 & precision@50
-    result[0] = sum(rel[:10]) / len(p_test)
-    result[1] = sum(rel[:50]) / len(p_test)
+    # recall@10 & recall@50
+    result[0] = sum(rel[:10]) / len(test)
+    result[1] = sum(rel[:50]) / len(test)
 
     # NDCG@100
     dcg = sum([v/log2(i+2) for i, v in enumerate(rel)])
@@ -59,37 +61,26 @@ def eval_playlist(ranked_songs, p_test, index_artist, index_track):
     return result
 
 
-def evaluate(playlists_with_embeddings, dist, num_epochs=1, test_to_seed_ratio=0.2, num_recs=100):
-    assert(100 <= num_recs <= playlists_with_embeddings.shape[0])
-
-    # combine playlist and embedding data into a single dataframe
-    # create new column to use when ranking songs
-    playlists_with_embeddings['score'] = 0
-    # holds scores (prec@10, prec@50, NDCG@100) for each playlist
+def evaluate(playlists, dist, output_path, num_epochs=5, test_to_seed_ratio=0.2, num_recs=100):
+    assert(100 <= num_recs <= playlists.shape[0])
     results = defaultdict(lambda: np.array([0.0, 0.0, 0.0]))
-    index_artist = playlists_with_embeddings.columns.get_loc('artist')
-    index_track = playlists_with_embeddings.columns.get_loc('track')
-    index_id = playlists_with_embeddings.columns.get_loc('sid')
-    seeds = [22, 90]
+    index_sid = playlists.columns.get_loc('sid')
+
     for n in range(num_epochs):
-        f = open('out/summ_epoch{}_frac{}.csv'.format(n, 0.05), 'w', newline="")
+        f = open(output_path + str(n) + ".csv", 'w', newline="")
         writer = csv.writer(f)
         step = 0
-        for pid, p in playlists_with_embeddings.groupby('playlist_id'):
-            step+=1
+        for pid, p in playlists.groupby('playlist_id'):
+            step += 1
             print("epoch " + str(n) + ", step " + str(step))
-            if p.shape[0] <= 1 : continue
-            p_seed, p_test = train_test_split(p.values.tolist(), test_size=test_to_seed_ratio, random_state=seeds[n])
-            ranked_songs = rank_songs(playlists_with_embeddings, dist, p_seed, index_id, num_recs)
-            result = np.array(eval_playlist(ranked_songs, p_test, index_artist, index_track))
+            if p.shape[0] <= 1:
+                continue
+            seed, test = train_test_split(p.values.tolist(), test_size=test_to_seed_ratio)
+            ranked_songs = rank_songs(playlists, dist, seed, index_sid, num_recs)
+            result = np.array(eval_playlist(ranked_songs, test, index_sid))
             results['pid'] += result
             writer.writerow(np.append(result, pid))
             f.flush()
-    # normalize scores by number of epochs
-    results = [tuple([i/num_epochs for i in v]) for v in results.values()]
-    # take average across all playlists
-    results = [sum(i)/len(i) for i in zip(*results)]
-    return results
 
 
 def main():
@@ -102,13 +93,20 @@ def main():
         a = np.array(embeddings)
         return cosine_similarity(a)
 
-    df1 = pd.read_csv('data/spotify_playlists_with_embeddings_summ.csv')
+    df1 = pd.read_csv('data/' + sys.argv[1])
+    df1['artist'] = df1['artist'].str.lower()
+    df1['track'] = df1['track'].str.lower()
 
-    df2 = pd.read_csv('data/spotify_summary_0.05_embeddings.csv')
+    df2 = pd.read_csv('data/' + sys.argv[2])
+    df2['sid'] = range(df2.shape[0])
     df2['embedding'] = df2.apply(embedding_proc, axis=1)
     dist = dist_matrix(df2['embedding'].values.tolist())
 
-    print(evaluate(df1, dist))
+    df3 = pd.merge(df2, df1, on=['artist', 'track'], how='left')
+
+    evaluate(df3, dist, output_path="out/")
+
 
 if __name__== "__main__":
     main()
+
